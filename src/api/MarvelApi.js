3b0f2f58409afc563320eab778ecf1f6380fc401
Cathereum <106606021+Cathereum@users.noreply.github.com>
2023-02-11 11:47:55 +0700

    spinner and character object generation completed

--- src/api/MarvelApi.js
+++ src/api/MarvelApi.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const _apiKey = "28f7af77830c4b5a79cdf17fef748cf1";
 
+//получаем всех персонажей с лимитом
 export const getAllCharacters = async () => {
   const srcData = await axios.get(
     `https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=210&apikey=${_apiKey}`
@@ -9,6 +10,7 @@ export const getAllCharacters = async () => {
   return srcData.data.data.results.map(_getCharacterObject);
 };
 
+//получаем 1 персонажа
 export const getUniqCharacter = async (characterId) => {
   const srcData = await axios.get(
     `https://gateway.marvel.com:443/v1/public/characters/${characterId}?apikey=${_apiKey}`
@@ -16,10 +18,14 @@ export const getUniqCharacter = async (characterId) => {
   return _getCharacterObject(srcData.data.data.results[0]);
 };
 
-export const _getCharacterObject = (character) => {
+//создаем объект 1 персонажа
+const _getCharacterObject = (character) => {
+  const noDescr = "There is no description for this character";
+  const yesDescr = `${character.description.slice(0, 150)}...`;
+
   return {
     name: character.name,
-    description: character.description,
+    description: !character.description ? noDescr : yesDescr,
     thumbnail: character.thumbnail.path + "." + character.thumbnail.extension,
     homepage: character.urls[0].url,
     wiki: character.urls[1].url,
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -3,8 +3,10 @@ import mjolnir from "../../resources/img/mjolnir.png";
 
 import { getUniqCharacter } from "../../api/MarvelApi";
 import { useEffect, useState } from "react";
+import Spinner from "../spinner/Spinner";
 
 const character = {};
+let loading = true;
 
 const RandomChar = () => {
   const [updateChar, setUpdateChar] = useState(character);
@@ -17,31 +19,13 @@ const RandomChar = () => {
 
   const showUniqCharacter = async (characterId) => {
     const uniqCharacter = await getUniqCharacter(characterId);
-
+    loading = false;
     return setUpdateChar(uniqCharacter);
   };
 
   return (
     <div className="randomchar">
-      <div className="randomchar__block">
-        <img
-          src={updateChar.thumbnail}
-          alt="Random character"
-          className="randomchar__img"
-        />
-        <div className="randomchar__info">
-          <p className="randomchar__name">{updateChar.name}</p>
-          <p className="randomchar__descr">{updateChar.description}</p>
-          <div className="randomchar__btns">
-            <a href={updateChar.homepage} className="button button__main">
-              <div className="inner">homepage</div>
-            </a>
-            <a href={updateChar.wiki} className="button button__secondary">
-              <div className="inner">Wiki</div>
-            </a>
-          </div>
-        </div>
-      </div>
+      {loading ? <Spinner /> : randomCharBlock(updateChar)}
       <div className="randomchar__static">
         <p className="randomchar__title">
           Random character for today!
@@ -53,6 +37,30 @@ const RandomChar = () => {
           <div className="inner">try it</div>
         </button>
         <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
+      </div>
+    </div>
+  );
+};
+
+const randomCharBlock = (updateChar) => {
+  return (
+    <div className="randomchar__block">
+      <img
+        src={updateChar.thumbnail}
+        alt="Random character"
+        className="randomchar__img"
+      />
+      <div className="randomchar__info">
+        <p className="randomchar__name">{updateChar.name}</p>
+        <p className="randomchar__descr">{updateChar.description}</p>
+        <div className="randomchar__btns">
+          <a href={updateChar.homepage} className="button button__main">
+            <div className="inner">homepage</div>
+          </a>
+          <a href={updateChar.wiki} className="button button__secondary">
+            <div className="inner">Wiki</div>
+          </a>
+        </div>
       </div>
     </div>
   );
