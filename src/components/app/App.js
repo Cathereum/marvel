4f223c3dd80db928c654635838f6a4b335e51cdb
Cathereum <106606021+Cathereum@users.noreply.github.com>
2023-02-14 20:38:39 +0700

    big update

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -6,14 +7,20 @@ import CharInfo from "../charInfo/CharInfo";
 import decoration from "../../resources/img/vision.png";
 
 const App = () => {
+  const [selectedChar, setChar] = useState(null);
+
+  const onCharSelected = (id) => {
+    setChar(id);
+  };
+
   return (
     <div className="app">
       <AppHeader />
       <main>
         <RandomChar />
         <div className="char__content">
-          <CharList />
-          <CharInfo />
+          <CharList onCharSelected={onCharSelected} />
+          <CharInfo charId={selectedChar} />
         </div>
         <img className="bg-decoration" src={decoration} alt="vision" />
       </main>
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,30 +1,53 @@
+import { useState, useEffect } from "react";
+import Spinner from "../spinner/Spinner";
+import MarvelService from "../../services/MarvelService";
+
 import "./randomChar.scss";
 import mjolnir from "../../resources/img/mjolnir.png";
 
-import { getUniqCharacter } from "../../api/MarvelApi";
-import { useEffect, useState } from "react";
-import Spinner from "../spinner/Spinner";
-
-const character = {};
-let loading = true;
 const RandomChar = () => {
-  const [updateChar, setUpdateChar] = useState(character);
+  const [char, setChar] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
-  const randomId = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+  const marvelService = new MarvelService();
 
   useEffect(() => {
-    showUniqCharacter(randomId);
+    updateChar();
+    const timerId = setInterval(updateChar, 60000);
+
+    return () => {
+      clearInterval(timerId);
+    };
   }, []);
 
-  const showUniqCharacter = async (characterId) => {
-    const uniqCharacter = await getUniqCharacter(characterId);
-    loading = false;
-    return setUpdateChar(uniqCharacter);
+  const onCharLoaded = (char) => {
+    setLoading(false);
+    setChar(char);
   };
+
+  const onCharLoading = () => {
+    setLoading(true);
+  };
+
+  const onError = () => {
+    setError(true);
+    setLoading(false);
+  };
+
+  const updateChar = () => {
+    const id = Math.floor(Math.random() * (1011400 - 1011000)) + 1011000;
+    onCharLoading();
+    marvelService.getCharacter(id).then(onCharLoaded).catch(onError);
+  };
+
+  const spinner = loading ? <Spinner /> : null;
+  const content = !(loading || error || !char) ? <View char={char} /> : null;
 
   return (
     <div className="randomchar">
-      {loading ? <Spinner /> : randomCharBlock(updateChar)}
+      {spinner}
+      {content}
       <div className="randomchar__static">
         <p className="randomchar__title">
           Random character for today!
@@ -32,10 +55,7 @@ const RandomChar = () => {
           Do you want to get to know him better?
         </p>
         <p className="randomchar__title">Or choose another one</p>
-        <button
-          onClick={() => showUniqCharacter(randomId)}
-          className="button button__main"
-        >
+        <button onClick={updateChar} className="button button__main">
           <div className="inner">try it</div>
         </button>
         <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
@@ -44,22 +64,32 @@ const RandomChar = () => {
   );
 };
 
-const randomCharBlock = (updateChar) => {
+const View = ({ char }) => {
+  const { name, description, thumbnail, homepage, wiki } = char;
+  let imgStyle = { objectFit: "cover" };
+  if (
+    thumbnail ===
+    "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+  ) {
+    imgStyle = { objectFit: "contain" };
+  }
+
   return (
     <div className="randomchar__block">
       <img
-        src={updateChar.thumbnail}
+        src={thumbnail}
         alt="Random character"
         className="randomchar__img"
+        style={imgStyle}
       />
       <div className="randomchar__info">
-        <p className="randomchar__name">{updateChar.name}</p>
-        <p className="randomchar__descr">{updateChar.description}</p>
+        <p className="randomchar__name">{name}</p>
+        <p className="randomchar__descr">{description}</p>
         <div className="randomchar__btns">
-          <a href={updateChar.homepage} className="button button__main">
+          <a href={homepage} className="button button__main">
             <div className="inner">homepage</div>
           </a>
-          <a href={updateChar.wiki} className="button button__secondary">
+          <a href={wiki} className="button button__secondary">
             <div className="inner">Wiki</div>
           </a>
         </div>
